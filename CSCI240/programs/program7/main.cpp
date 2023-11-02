bdc1399995b81d4f4ddf882df14a0bc042b24199
mattgithub2 <mrpizzaman856@gmail.com>
2023-11-01 19:24:53 -0500

    dailies

--- CSCI240/programs/program7/main.cpp
+++ CSCI240/programs/program7/main.cpp
@@ -13,17 +13,38 @@ Purpose:
 #include <iomanip>
 #include <fstream>
 #include <cstdlib>
+#include <cmath>
+#include <ctime>
+#include <string>
+#include <vector>
+#include <cctype>
+
 using namespace std;
+
+const int SIZE_ARRAY = 30;
+const int COLUMN_WIDTH = 16;
+
+// function prototypes
+int build(string[], string[], int[], int[], int[]);
+void print(string[], string[], int[], int[], int[], int);
 
 int main(){
   
   // array initilization
-  string seasonNames[24];
-  string cropNames[24];
-  int seedCosts[24];
-  int saleValues[24];
-  int numberOfDays[24];
- 
+  string seasonNames[SIZE_ARRAY];
+  string cropNames[SIZE_ARRAY];
+  int seedCosts[SIZE_ARRAY];
+  int saleValues[SIZE_ARRAY];
+  int numberOfDays[SIZE_ARRAY];
+  
+  int cropAmount = build(seasonNames, cropNames, seedCosts, saleValues, numberOfDays);
+  print(seasonNames, cropNames, seedCosts, saleValues, numberOfDays, cropAmount);
+  
+  
+  return 0;
+}
+int build(string season[], string name[], int seed_cost[], int sell_value[], int harvest_time[]){
+
   ifstream infile; // input file stream variable
   
   infile.open("crops.txt"); // open the file for reading
@@ -35,7 +56,7 @@ int main(){
 
   // variables that temperarily store the data before adding it to our arrays
   string season_name, crop_name; 
-  int seed_cost, sale_value, days;
+  int seedCost, saleValue, Days;
 
   int index = 0; // indices for the the parallel arrays
   
@@ -43,25 +64,54 @@ int main(){
   while (infile){ // reads data from file and stores in into the corresponding arrays
     
     infile >> crop_name;
-    infile >> seed_cost;
-    infile >> sale_value;
-    infile >> days;
+    infile >> seedCost;
+    infile >> saleValue;
+    infile >> Days;
 
     // adding data to arrays 
-    seasonNames[index] = season_name; 
-    cropNames[index] = crop_name;
-    seedCosts[index] = seed_cost;
-    saleValues[index] = sale_value;
-    numberOfDays[index] = days;
+    season[index] = season_name; 
+    name[index] = crop_name;
+    seed_cost[index] = seedCost;
+    sell_value[index] = saleValue;
+    harvest_time[index] = Days;
 
     index++; // next subscript
     
     infile >> season_name; // secondary read
   }
   infile.close(); // closing file
-  
-  return 0;
+
+  int amountOfCrops = index; 
+
+ return amountOfCrops; 
+}
+
+void print(string season[], string name[], int seed_cost[], int sell_value[], int harvest_time[], int num_of_crops){
+
+  cout << setw(30) << right << "" << "Gold Profit Per Day Report for All Crops" << endl <<endl;
+  cout << setw(COLUMN_WIDTH) << right << "Season" << setw(COLUMN_WIDTH) << right << "Crop Name" << setw(COLUMN_WIDTH) << right << "Seed Cost" << setw(COLUMN_WIDTH) << right << "Sell Value" << setw(COLUMN_WIDTH) << right << "Harvest Time" << setw(COLUMN_WIDTH) << right << "Gold Profit/Day" << endl;
+  for (int i = 0; i < 96; i++){
+    cout << "-";
+  }
+  cout << endl;
+  for (int i = 0; i < num_of_crops; i++){
+    double profit = (double)(sell_value[i] - seed_cost[i]) / harvest_time[i]; // performing profit calculation
+
+
+    cout << setw(COLUMN_WIDTH) << right << season[i];
+    cout << setw(COLUMN_WIDTH) << right << name[i];
+    cout << setw(COLUMN_WIDTH) << right << seed_cost[i];
+    cout << setw(COLUMN_WIDTH) << right << sell_value[i];
+    cout << setw(COLUMN_WIDTH) << right << harvest_time[i];
+    cout << setw(COLUMN_WIDTH) << right << setprecision(2) << fixed << profit;
+    cout << endl;
+  }
 }
 void sort(string season[], string name[], int seed_cost[], int sell_value[], int harvest_time[], int num_of_crops){
-
+  for (int i = num_of_crops - 1; i > 0; i--){
+    int min = i;
+    for (int j = i - 1; j >= 0; j--){
+      if ()
+    }
+  }
 }
