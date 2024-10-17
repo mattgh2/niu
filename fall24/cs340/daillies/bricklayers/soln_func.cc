96405d8b7971038c4492d601a8eca43af4db0b13
mattgithub2 <mattwarner744@gmail.com>
2024-10-17 13:35:52 -0500

    ...

--- fall24/cs340/daillies/bricklayers/soln_func.cc
+++ fall24/cs340/daillies/bricklayers/soln_func.cc
@@ -1,8 +1,4 @@
 #include <algorithm>
-#include <cmath>
-#include <numeric>
-#include <iostream>
-
 int r_bricklayers(int b, int * first, int * last,  int len) {
     if (first >= last) { return -1; }
     int walls = len / b;
@@ -13,18 +9,10 @@ int r_bricklayers(int b, int * first, int * last,  int len) {
     return std::max(time, r_bricklayers(b - 1, first, last - 1, len - walls));
 }
 int bricklayers(int b, int a[], int len) {
-
     if (b <= 0 || len <= 0) {return -1; }
-    if (b >= len) { 
-        int time = *std::max_element(a, a + len);
-        return (time) ? time : -1; 
-    }
-    if (b == 1) { 
-        int acc = std::accumulate(a, a + len, 0);
-        return (acc) ? acc : -1;
-    }
     std::sort(a, a + len);
     int time = (r_bricklayers(b, a, a + len - 1, len));
     return (time > 0) ? time : -1;
 
 }
+
