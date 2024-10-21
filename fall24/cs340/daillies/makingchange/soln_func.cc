aaa3011624e27e03e783fd0de603b7eb27a34628
mattgithub2 <mattwarner744@gmail.com>
2024-10-21 12:32:55 -0500

    ...

--- fall24/cs340/daillies/makingchange/soln_func.cc
+++ fall24/cs340/daillies/makingchange/soln_func.cc
@@ -1,22 +1,28 @@
 #include <vector>
-#include <numeric>
-// combinations of that sum to c. (with repeats)
-auto _r(int c, int a[], int start, std::vector<int> &cur, std::vector<std::vector<int>> &sol, int N) {
-    if (c == 0) { sol.push_back(cur); }
-    if (c < 0) { return; }
+#include <set>
+
+struct size_comp {
+    bool operator()(const std::vector<int> &a, const std::vector<int> &b) const {
+        return a.size() < b.size();
+    }
+};
+auto _r(int c, int a[], int start, std::vector<int> cur, int N) -> std::set<std::vector<int>, size_comp> {
+    std::set<std::vector<int>, size_comp> s;
+    if (c == 0) { s.insert(cur); return s; }
+    if (c < 0) { return {}; }
     for (int i = start; i < N; ++i) {
         cur.push_back(a[i]);
-        _r(c - a[i], a, i, cur, sol, N);
+        auto below = _r(c - a[i], a, i, cur, N);
+        s.insert(below.begin(), below.end());
         cur.pop_back();
     }
+    return s;
 }
+
 auto mintokens(int c, int a[], int len) -> int {
-    if (len < 1 || c < 1)  {return -1; }
-    std::vector<int> h;
-    std::vector<std::vector<int>> k;
-    _r(c, a, 0, h,k, len);
-    if (k.size() == 0) {return -1 ;}
-    return std::accumulate(k.begin() + 1, k.end(), (*k.begin()).size(), [](auto a, const auto &b) {
-                if (b.size() < a)  { return b.size(); }
-                return a; }); 
+    if (len < 1 || c < 1)  { return -1; }
+    auto solution = _r(c, a, 0, {}, len);
+    if (solution.empty()) { return -1 ;}
+    int _size =  (*solution.begin()).size();
+    return _size;
 }
